024e5e1a00a18629332d23acb00ebe45883397ac
Dan Bridges <danieljbridges@gmail.com>
2024-09-16 14:40:05 +0200

    Better layout and handling

--- scripts/update_repos.sh
+++ scripts/update_repos.sh
@@ -2,13 +2,19 @@
 # NOMADS repos update script
 # Written by Daniel Bridges (dbscientificltd@gmail.com)
 
-condabase=$(conda info --all | grep "base environment" | sed s/"base environment : "// | sed s/\(writable\)// | sed s/\s+//)
+#Generate the necessary for
+condabase="$(conda info --all | grep "base environment" | sed s/"base environment : "// | sed s/\(writable\)// | sed s/\s+//)"
+condapath="/etc/profile.d/conda.sh"
+condash=$(echo $condabase$condapath | sed s/[[:space:]]//)
+
+divider="\n\n***************************************\n"
 
 function change_conda {
     #source the conda executable
-    source $condabase/etc/profile.d/conda.sh
+    echo "Changing from $CONDA_DEFAULT_ENV env to $1."
+    source $condash
     conda activate $1
-
+    echo "   $CONDA_DEFAULT_ENV"
     if [[ $CONDA_DEFAULT_ENV != $1 ]]; then
         echo "Failed to enter correct conda env"
         exit
@@ -24,7 +30,7 @@ function change_git_origin_between_https_ssh {
 
     CURRENT=$(git remote get-url origin | awk -F'[://@]' '{print $1}' | sed s/git/ssh/)
     GITSTUB=$(git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://' | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')
-    #echo "current: $CURRENT args: $1"
+
     #Remove those that match
     if [[ "$CURRENT" = "$1" ]]; then
         return
@@ -61,6 +67,9 @@ done
 # Shift away processed options from remaining arguments
 shift $((OPTIND - 1))
 
+echo "Searching $gitdir for git folders:"
+printf $divider
+
 #Identify all likely dir in the git_dir
 find "$git_dir" -mindepth 1 -maxdepth 1 -type d -not -path '*/.*' | while read dir; do
 
@@ -70,7 +79,7 @@ find "$git_dir" -mindepth 1 -maxdepth 1 -type d -not -path '*/.*' | while read d
     fi
 
     repo=$(basename "$dir")
-    echo "Attempting to update $repo"
+    echo "Found $repo. Attempting to update:"
     cd $dir
 
     #Ensure that it is using an https
@@ -80,41 +89,46 @@ find "$git_dir" -mindepth 1 -maxdepth 1 -type d -not -path '*/.*' | while read d
 
     output=$(git pull)
     status=$?
-
     # Highlight those that failed
     if ! [[ $status -eq 0 ]]; then
-        echo "Git pull failed."
-        echo "   $output"
+        echo "   ERROR: Git pull failed."
+        echo "      $output"
+        printf $divider
         continue
     fi
 
     #Finish with those that are up to date
     if [[ "$output" == "Already up to date." ]]; then
         echo "   $output"
+        printf $divider
         continue
     fi
 
     # Check if there is a README file
     readme="README.md"
     if [ ! -f $readme ]; then
-        echo "Error: File 'README.md' does not exist."
+        echo "   Error: File 'README.md' does not exist."
         exit 1
     fi
 
     #Search for install phrase
     phrase="pip install -e ."
     if ! grep -q "$phrase" "$readme"; then
-        echo "No pip install required"
+        echo "   No pip install required"
+        printf $divider
         continue
     fi
+
     #Check environment exists
     env_present=$(conda info --envs | grep warehouse | wc -l)
     if [[ $env_present -eq "1" ]]; then
         #Change into the env
         change_conda "$repo"
-        #pip install -e .
+        pip install -e .
+        printf $divider
     else
-        echo "$repo not installed. Please install and then re-run this script"
+        echo "   $repo not installed. Please install and then re-run this script"
     fi
 
+    printf $divider
 done
