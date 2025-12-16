a61ee1931216ffc59fbcf73d50c99c7fd3fef508
Daniel Bridges <danieljbridges@gmail.com>
2025-12-16 15:04:07 +0200

    Remove unnecessary install and update scripts

--- src/resources/lib/exceptions.py
+++
@@ -1,8 +0,0 @@
-from resources.lib.general import list_repository_details
-
-
-class InputError(Exception):
-    """Failure to identify path"""
-
-    list_repository_details(list_tools, repos)
-    pass
